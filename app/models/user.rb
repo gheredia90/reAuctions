1d6d43d4dd8bde1a9cb4f69b75c5e26793872243
gheredia90 <gheredia28@gmail.com>
2016-03-04 21:27:02 +0100

    User model updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,19 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+	# Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable and :omniauthable
+    devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+    has_many :answers, as: :supplier
+    has_many :rfps
 
-def rfps_as_buyer
-	Rfp.where(buyer_id: self.id)
-end
+	def rfps_as_buyer
+		Rfp.where(buyer_id: self.id)
+	end
 
 
-def rfps_as_supplier
-	Rfp.join(:answers).where(supplier_id: self.id).uniq
-end
-
+	def rfps_as_supplier
+		Rfp.joins(:answers).where('answers.supplier_id' => self.id)
+	end
 
 end
