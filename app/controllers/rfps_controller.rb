74b4ed928725ac1d2824ab13aa2235c6466b4200
gheredia90 <gheredia28@gmail.com>
2016-03-23 17:56:30 +0100

    RFP creation/Edit updated

--- app/controllers/rfps_controller.rb
+++ app/controllers/rfps_controller.rb
@@ -16,6 +16,7 @@ class RfpsController < ApplicationController
 	def create
 		@rfp = Rfp.new rfp_params
 		@rfp.buyer = current_user
+		params[:questions].each_value {|value| @rfp.questions << Question.find_by_id(value)} 
 		if @rfp.save
 			flash[:notice] = "RFP successfully created"
 			redirect_to dashboard_path			
@@ -31,6 +32,7 @@ class RfpsController < ApplicationController
 	def update
 		@rfp = Rfp.find params[:id]
 		if @rfp.update_attributes rfp_params
+			@rfp.update_questions!(params[:questions])
 			flash[:notice] = "Entry updated successfully"
 			redirect_to dashboard_path
 		else
@@ -38,8 +40,15 @@ class RfpsController < ApplicationController
 			render 'edit'
 		end
 	end
+		
 
 	def send_answers
+		@rfp = Rfp.find_by_id(params[:id])
+		current_user.answers_sent?(@rfp) ? update_answers : create_answers
+		redirect_to dashboard_path		
+	end
+
+	def create_answers
 		params[:answers].each do |key, value|
 			@answer = Answer.new
 			@answer.question_id = value["question"].to_f
@@ -48,19 +57,18 @@ class RfpsController < ApplicationController
 			@answer.supplier_id = current_user.id
 			@answer.save
 		end
-		redirect_to dashboard_path		
-	end
+	end	
 
 	def update_answers
 		params[:answers].each do |key, value|
-			@answer = Answer.where(question_id: value["question"].to_f)			
-			@answer.text = value["text"]			
+			@answer = Answer.where("supplier_id = ? and rfp_id = ? and question_id = ?", current_user.id, value["rfp"].to_f, value["question"].to_f)[0]
+			@answer.text= value["text"]		
 			@answer.save
 		end
-		redirect_to dashboard_path		
 	end
 
     def rfp_params
 		params.require(:rfp).permit(:title, :description, :category)
 	end
+
 end
--- app/models/rfp.rb
+++ app/models/rfp.rb
@@ -2,4 +2,24 @@ class Rfp < ActiveRecord::Base
 	has_and_belongs_to_many :questions
 	has_many :answers
 	belongs_to :buyer, class_name: "User"
+
+	def update_questions!(questions)
+		puts "----------------------------------------------------------------"
+		puts questions.size > 0
+		puts "----------------------------------------------------------------"
+		if questions.size > 0
+			questions.each_value {|value| self.add_question(value)}
+			self.save
+		end
+	end	
+
+	def add_question(value)
+		question = Question.find_by_id(value)
+		puts "----------------------------------------------------------------"
+		puts question.in?(self.questions)
+		puts "----------------------------------------------------------------"
+		unless question.in?(self.questions)
+			self.questions << question
+		end
+	end	
 end
